fb9e112ce31c1b78607016a10878e761a77fbf25
Polawat Ruenruay <pruenrua@z3t1c1.42bangkok.com>
2023-01-07 17:19:47 +0700

    1st step to read

--- get_next_line.c
+++ get_next_line.c
@@ -4,6 +4,30 @@
 
 char *search_for_the_lie(int fd, char *hermem)
 {
+  char *buffer;
+  char *tmp;
+  //create the mem if not valid that can use in strjoin function
+
+  if(!hermem)
+    hermem = malloc(1 * sizeof(char));
+  if(!hermem)
+    return(printf("\ni cant malloc----------------------- "));
+  buffer = malloc(BUFFER_SIZE + 1 * sizeof(char));
+  if(buffer)
+    buffer[BUFFER_SIZE] = '\0';
+  else
+    return(0);
+  
+  while(read(fd, buffer, BUFFER_SIZE)) // just read !!!!! <-------- must be add the \n checker later
+  {
+    tmp = hermem;
+    printf("\nbefore join buffer is %s \nhermem is %s" , buffer,hermem);
+    hermem = ft_strjoin(hermem, buffer);
+    free(tmp);
+  }
+  free(buffer);
+
+  return(hermem);
   
 }
 
@@ -22,18 +46,18 @@ char *get_next_line(int fd)
   static char *hermem;
   char *result;
   
-  if(fd < 0 || BUFFERSIZE <= 0)
+  printf("start with BUFFERSIZE > %d \n", BUFFER_SIZE);
+  if(fd < 0 || BUFFER_SIZE <= 0)
     return(0);
   hermem = search_for_the_lie(fd , hermem); // do the read function until found the \n in the line and return
   // return null if cant malloc or error
-  
+  printf("\nhemem is '%s'\n exit...", hermem);
   if(!hermem)
     return (0);
   
-  result = get_next_lie(hermem); //get the thing before \n to it malloc also
-  hermem = ready_for_next_lie(hermem); //move to after \n free old and malloc after it
-
-  return(result);
+  //result = get_next_lie(hermem); //get the thing before \n to it malloc also
+  //hermem = ready_for_next_lie(hermem); //move to after \n free old and malloc after it
+  return(hermem);
 }
 
 
@@ -46,7 +70,7 @@ int main()
   while(i)
   {
     printf("--------------------[%d]-----------------\n", i);
-    sumstr = str_GNL_MK2(fd);
+    sumstr = get_next_line(fd);
     
 	  printf("final is \n%s", sumstr);
     printf("--------------------------------------\n");
